c7e4db41de8705af6486c74398f80a859611e14b
Simbamon <sbhan9511@gmail.com>
2021-12-25 20:45:38 +0900

    connected mongo

--- app.js
+++ app.js
@@ -3,11 +3,14 @@ const app = express()
 const port = 5001
 const mongoose = require('mongoose')
 const path = require('path')
-
+const bodyParser = require('body-parser')
+const cors = require('cors')
 
 require('dotenv').config({ path: path.resolve(__dirname, './.env') })
 process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
 
+app.use(cors())
+app.use(bodyParser.json())
 app.use(express.static(path.join(__dirname, 'build')))
 
 app.get('/', function(req, res) {
@@ -19,6 +22,12 @@ const watsonmlRoute = require('./routes/watsonml')
 
 app.use('/wkc', wkcRoute)
 app.use('/watsonml', watsonmlRoute)
+
+mongoose.connect(
+    process.env.DB_CONNECTION,
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    () => console.log('connected to db')
+)
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
