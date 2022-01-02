0d2926ed246e4f64d66a84db0992ebb1024f862d
Simbamon <sbhan9511@gmail.com>
2022-01-02 17:37:08 +0900

    testing redis deployment

--- routes/wkc.js
+++ routes/wkc.js
@@ -82,26 +82,36 @@ router.post("/getcatalogs", async (req, res) => {
 })
 
 router.post("/getcataloginfo/:catalogID", async (req, res) => {
-    console.log("/getcataloginfo endpoint called")
-    const WKC_TOK = req.body.token
-    const CP4D_CATALOG_ID = req.params.catalogID
-    console.log(CP4D_CATALOG_ID)
-    const url = cp4durl + `v2/catalogs/` + CP4D_CATALOG_ID
-    const response = await fetch(url, {
-        method: 'GET',
-        headers: {
-            "Authorization": "Bearer " + WKC_TOK
+    redisClient.get("cataloginfo", async(error, cataloginfo) => {
+        if(error) throw error
+        if(cataloginfo !== null) {
+            console.log("Catalog Info Cache Hit")
+            return res.json(JSON.parse(cataloginfo))
+        }
+        else{
+            console.log("/getcataloginfo endpoint called")
+            const WKC_TOK = req.body.token
+            const CP4D_CATALOG_ID = req.params.catalogID
+            console.log(CP4D_CATALOG_ID)
+            const url = cp4durl + `v2/catalogs/` + CP4D_CATALOG_ID
+            const response = await fetch(url, {
+                method: 'GET',
+                headers: {
+                    "Authorization": "Bearer " + WKC_TOK
+                }
+            })
+            .then(res => res.text())
+            .catch(e => {
+                console.error({
+                    "message": "Error",
+                    error: e,
+                })
+                })
+            console.log("RESPONSE: ", response)
+            redisClient.setex("cataloginfo", 1800, response)
+            res.send(response)
         }
     })
-    .then(res => res.text())
-    .catch(e => {
-        console.error({
-            "message": "Error",
-            error: e,
-        })
-        })
-    console.log("RESPONSE: ", response)
-    res.send(response)
 })
 
 router.post("/getassetlist", async (req, res)=> {
@@ -132,57 +142,77 @@ router.post("/getassetlist", async (req, res)=> {
 })
 
 router.post("/getassetlistbyreview/:catalogID", async (req, res)=> {
-    console.log("Getting asset list by review...")
-    const WKC_TOK = req.body.token
-    const CP4D_CATALOG_ID = req.params.catalogID
-    const url = cp4durl + `v2/asset_types/asset/search?catalog_id=` + CP4D_CATALOG_ID
-    const response = await fetch(url, {
-        method: 'POST',
-        headers: {
-            "Authorization": "Bearer " + WKC_TOK,
-            'Content-Type': 'application/json',
-            'Cache-Control': 'no-cache'
-        },
-        body: JSON.stringify({
-            "limit": 5,
-            "sort": "-asset.rating",
-            "query": "(*:* OR *:*) AND NOT asset.asset_type:\"activity\" AND NOT asset.asset_type:\"activity_run\" AND NOT asset.asset_type:\"data_flow_run\" AND NOT asset.asset_type:\"data_flow\" AND NOT asset.description:\"Automatically transformed asset\" AND NOT asset.description:\"IBM COS Connection for Data Profile summary\" AND NOT asset.asset_category:\"SYSTEM\" AND asset.project_id:0",
-            "include": "entity"
-        })
+    redisClient.get("getassetlistbyreview", async(error, getassetlistbyreview) => {
+        if(error) throw error
+        if(getassetlistbyreview !== null) {
+            console.log("Asset list(review) Cache Hit")
+            return res.json(JSON.parse(getassetlistbyreview))
+        }
+        else{
+            console.log("Getting asset list by review...")
+            const WKC_TOK = req.body.token
+            const CP4D_CATALOG_ID = req.params.catalogID
+            const url = cp4durl + `v2/asset_types/asset/search?catalog_id=` + CP4D_CATALOG_ID
+            const response = await fetch(url, {
+                method: 'POST',
+                headers: {
+                    "Authorization": "Bearer " + WKC_TOK,
+                    'Content-Type': 'application/json',
+                    'Cache-Control': 'no-cache'
+                },
+                body: JSON.stringify({
+                    "limit": 5,
+                    "sort": "-asset.rating",
+                    "query": "(*:* OR *:*) AND NOT asset.asset_type:\"activity\" AND NOT asset.asset_type:\"activity_run\" AND NOT asset.asset_type:\"data_flow_run\" AND NOT asset.asset_type:\"data_flow\" AND NOT asset.description:\"Automatically transformed asset\" AND NOT asset.description:\"IBM COS Connection for Data Profile summary\" AND NOT asset.asset_category:\"SYSTEM\" AND asset.project_id:0",
+                    "include": "entity"
+                })
+            })
+            .then(res => res.text())
+            .catch(e => {
+                console.error({
+                    "message": "Error",
+        
+                    error: e,
+                })
+                })
+            console.log("RESPONSE: ", response)
+            redisClient.setex("getassetlistbyreview", 1800, response)
+            res.send(response)
+        }        
     })
-    .then(res => res.text())
-    .catch(e => {
-        console.error({
-            "message": "Error",
-
-            error: e,
-        })
-        })
-    console.log("RESPONSE: ", response)
-    res.send(response)
 })
 
 router.post("/getassetmeta", async (req, res)=> {
-    console.log("Getting asset's meta data...")
-    const WKC_TOK = req.body.token
-    const CP4D_CATALOG_ID = req.body.catalog_id
-    const CP4D_ASSET_ID = req.body.asset_id
-    const url = cp4durl + `v2/assets/` + CP4D_ASSET_ID + `?catalog_id=` + CP4D_CATALOG_ID
-    const response = await fetch(url, {
-        method: 'GET',
-        headers: {
-            "Authorization": "Bearer " + WKC_TOK
+    redisClient.get("getassetmeta", async(error, getassetmeta) => {
+        if(error) throw error
+        if(getassetmeta !== null) {
+            console.log("Asset Meta Cache Hit")
+            return res.json(JSON.parse(getassetmeta))
         }
+        else{
+            console.log("Getting asset's meta data...")
+            const WKC_TOK = req.body.token
+            const CP4D_CATALOG_ID = req.body.catalog_id
+            const CP4D_ASSET_ID = req.body.asset_id
+            const url = cp4durl + `v2/assets/` + CP4D_ASSET_ID + `?catalog_id=` + CP4D_CATALOG_ID
+            const response = await fetch(url, {
+                method: 'GET',
+                headers: {
+                    "Authorization": "Bearer " + WKC_TOK
+                }
+            })
+            .then(res => res.text())
+            .catch(e => {
+                console.error({
+                    "message": "Error",
+                    error: e,
+                })
+                })
+            console.log("RESPONSE: ", response)
+            redisClient.setex("getassetmeta", 1800, response)
+            res.send(response)
+        }        
     })
-    .then(res => res.text())
-    .catch(e => {
-        console.error({
-            "message": "Error",
-            error: e,
-        })
-        })
-    console.log("RESPONSE: ", response)
-    res.send(response)
 })
 
 router.post("/getassetreview", async (req, res)=> {
