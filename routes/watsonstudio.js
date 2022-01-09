72163a6ad3dc0bef813d4dec213fd9f68e8d9c19
Simbamon <sbhan9511@gmail.com>
2022-01-09 12:30:39 +0900

    watson studio api work

--- routes/watsonstudio.js
+++ routes/watsonstudio.js
@@ -2,58 +2,72 @@ const express = require('express');
 const router = express.Router();
 const path = require('path')
 const fetch = require('node-fetch')
-const redis = require('redis')
 // const util = require('util')
 
 require('dotenv').config({ path: path.resolve(__dirname, './.env') })
 router.use(express.json())
 
 const cp4dwmlurl = process.env.WMLURL
-const wml_uname = process.env.WMLUSERNAME
-const wml_password = process.env.WMLPASSWORD
-const redisHost = process.env.REDISHOST
-const redisPort = process.env.REDISPORT
-const redisPassword = process.env.REDISPASSWORD
 
-const redisClient = redis.createClient({
-    host: redisHost,
-    port: redisPort,
-    password: redisPassword
+router.post("/wstoken", async(req, res) => {
+    console.log("Getting Watson Studio token")
+    const uname = req.body.uname
+    const upassword = req.body.upassword
+    const url = cp4dwmlurl + `icp4d-api/v1/authorize`
+    const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+            "Cache-Control": "no-cache",
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            username: uname,
+            password: upassword
+        })
+    })
+    .then(res => res.text())
+    .catch(e => {
+        console.error({
+            "message": "Error",
+            error: e,
+        })
+        })
+    // console.log("RESPONSE: ", response)
+    res.send(response)  
 })
 
-router.get("/wstoken", async(req, res) => {
-    redisClient.get("wstoken", async(error, token) => {
-        if(error) throw error
-        if(token !== null) {
-            console.log("Watson Studio Token Cache Hit")
-            return res.json(JSON.parse(token))
-        }
-        else{
-            console.log("Getting Watson Studio token")
-            const url = cp4dwmlurl + `icp4d-api/v1/authorize`
-            const response = await fetch(url, {
-                method: 'POST',
-                headers: {
-                    "Cache-Control": "no-cache",
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify({
-                    username: wml_uname,
-                    password: wml_password
-                })
-            })
-            .then(res => res.text())
-            .catch(e => {
-                console.error({
-                    "message": "Error",
-                    error: e,
-                })
-                })
-            // console.log("RESPONSE: ", response)
-            redisClient.setex("wstoken", 1800, response)
-            res.send(response)                        
-        }
+router.post('/createproject', async (req, res) => {
+    console.log("Creating Watson Studio Project")
+    const AuthToken = req.body.token
+    const url = cp4dwmlurl + `transactional/v2/projects`
+    const username = req.body.uname
+    const projectName = req.body.project_title
+    const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + AuthToken
+        },
+        body: JSON.stringify({
+            name: projectName,
+            description: username + "의 프로젝트입니다.",
+            public: false,
+            enforce_members: true,
+            storage: {
+                type: "assetfiles",
+                guid: "aa817103-b634-4306-b412-7b96d9802564"
+    }
+        })
     })
+    .then(res => res.text())
+    .catch(e => {
+        console.error({
+            "message": "Error",
+            error: e,
+        })
+        })
+    console.log("RESPONSE: ", response)
+    res.send(response)
 })
 
 module.exports = router
