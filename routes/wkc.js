15a7540b7b32224e6381750008eeab8d66692762
Simbamon <sbhan9511@gmail.com>
2022-04-02 12:29:17 +0900

    Eng change

--- routes/wkc.js
+++ routes/wkc.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const path = require('path')
 const fetch = require('node-fetch')
 const redis = require('redis')
+const util = require('util')
 
 require('dotenv').config({ path: path.resolve(__dirname, './.env') })
 process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
@@ -60,6 +61,31 @@ const redisClient = redis.createClient({
     
 // })
 
+router.get("/token", async(req, res) => {
+    console.log("Getting WKC token")
+    const url = cp4durl + `icp4d-api/v1/authorize`
+    const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+            "Cache-Control": "no-cache",
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            username: uname,
+            password: password
+        })
+    })
+    .then(res => res.text())
+    .catch(e => {
+        console.error({
+            "message": "Error",
+            error: e,
+        })
+        })
+    console.log("RESPONSE: ", response)
+    res.send(response)    
+})
+
 router.post("/getcatalogs", async (req, res) => {
     console.log("/getcatalog endpoint called")
     const WKC_TOK = req.body.token
@@ -116,25 +142,26 @@ router.post("/getcatalogs", async (req, res) => {
 
 router.post("/getcataloginfo/:catalogID", async (req, res) => {
     console.log("/getcataloginfo endpoint called")
-            const WKC_TOK = req.body.token
-            const CP4D_CATALOG_ID = req.params.catalogID
-            console.log(CP4D_CATALOG_ID)
-            const url = cp4durl + `v2/catalogs/` + CP4D_CATALOG_ID
-            const response = await fetch(url, {
-                method: 'GET',
-                headers: {
-                    "Authorization": "Bearer " + WKC_TOK
-                }
-            })
-            .then(res => res.text())
-            .catch(e => {
-                console.error({
-                    "message": "Error",
-                    error: e,
-                })
-                })
-            console.log("RESPONSE: ", response)
-            res.send(response)
+    console.log(util.inspect(req.body, false, null, true))
+    const WKC_TOK = req.body.wkc_token
+    const CP4D_CATALOG_ID = req.params.catalogID
+    console.log(CP4D_CATALOG_ID)
+    const url = cp4durl + `v2/catalogs/` + CP4D_CATALOG_ID
+    const response = await fetch(url, {
+        method: 'GET',
+        headers: {
+            "Authorization": "Bearer " + WKC_TOK
+        }
+    })
+    .then(res => res.text())
+    .catch(e => {
+        console.error({
+            "message": "Error",
+            error: e,
+        })
+        })
+    console.log("RESPONSE: ", response)
+    res.send(response)
 })
 
 
@@ -142,7 +169,7 @@ router.post("/getcataloginfo/:catalogID", async (req, res) => {
 router.post("/getassetlist", async (req, res)=> {
     console.log("Getting asset list...")
     const WKC_TOK = req.body.token
-    const url = cp4durl + `v2/asset_types/asset/search?catalog_id=64c25f35-eefb-4172-b6c3-38d8492fb4bb`
+    const url = cp4durl + `v2/asset_types/asset/search?catalog_id=c315ddd1-5711-46c2-b6e5-e919c089817d`
     const response = await fetch(url, {
         method: 'POST',
         headers: {
@@ -211,7 +238,7 @@ router.post("/getassetlist", async (req, res)=> {
 
 router.post("/getassetlistbyreview/:catalogID", async (req, res)=> {
     console.log("Getting asset list by review...")
-            const WKC_TOK = req.body.token
+            const WKC_TOK = req.body.wkc_token
             const CP4D_CATALOG_ID = req.params.catalogID
             const url = cp4durl + `v2/asset_types/asset/search?catalog_id=` + CP4D_CATALOG_ID
             const response = await fetch(url, {
@@ -277,7 +304,7 @@ router.post("/getassetlistbyreview/:catalogID", async (req, res)=> {
 
 router.post("/getassetmeta", async (req, res)=> {
     console.log("Getting asset's meta data...")
-            const WKC_TOK = req.body.token
+            const WKC_TOK = req.body.wkc_token
             const CP4D_CATALOG_ID = req.body.catalog_id
             const CP4D_ASSET_ID = req.body.asset_id
             const url = cp4durl + `v2/assets/` + CP4D_ASSET_ID + `?catalog_id=` + CP4D_CATALOG_ID
